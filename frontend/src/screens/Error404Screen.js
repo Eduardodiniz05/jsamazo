b7f1d642dd4910cd07f69a49d5623fdc8bb2cd97
Eduardodiniz05 <eduardo.diniz005@gmail.com>
2021-11-22 17:57:18 -0300

    adicionado error404screen.js, productscreeen.js e utils.js

---
+++ frontend/src/screens/Error404Screen.js
@@ -0,0 +1,7 @@
+const Error404Screen = {
+    render: () => {
+        return `<div>Pagina não encontrada! </div>`;
+    },
+}
+
+export default Error404Screen;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,7 @@
+const ProductScreen = {
+    render: () => {
+        return `<div>Tela de produto</div>`;
+    },
+}
+
+export default ProductScreen;
